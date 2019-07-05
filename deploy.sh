97dbb770d3cdec9e2ab3e7aca7465cf0d0bc066e
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2019-07-05 12:44:09 +0300

    Updated docker image names. Created deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git reset --hard
+git fetch --prune
+git checkout master
+git pull origin master
+docker-compose exec movierama_php composer install --optimize-autoloader --no-dev
+docker-compose exec movierama_php php artisan migrate --force
+docker-compose exec movierama_php php artisan route:cache
+docker-compose exec movierama_php php artisan config:cache
+docker-compose exec movierama_php php artisan view:clear
