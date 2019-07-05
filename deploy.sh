11ffea583777653ecc000657e1940f653b9d106d
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2019-07-05 12:48:27 +0300

    Updated deploy.hs

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git fetch --prune
 git checkout master
 git pull origin master
 docker-compose exec movierama_php composer install --optimize-autoloader --no-dev
-docker-compose exec movierama_php php artisan migrate --force
 docker-compose exec movierama_php php artisan route:cache
 docker-compose exec movierama_php php artisan config:cache
 docker-compose exec movierama_php php artisan view:clear
+docker-compose exec movierama_php php artisan migrate --force
